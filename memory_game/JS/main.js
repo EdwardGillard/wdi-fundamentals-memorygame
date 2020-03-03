c97ed2215e66dcdffed4fcfec21d3b8f6bf85048
Edward Gillard <edwardgillard@edwards-mbp.mynet>
2020-03-03 21:21:02 +0000

    Created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,20 +1,45 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{ 
+  rank: "Queen",
+  suit: "Hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{ 
+  rank: "Queen",
+  suit: "Diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank:"King",
+  suit:"Hearts",
+  cardImage:"images/king-of-hearts.png"
+},
+{
+  rank:"King",
+  suit:"Diamonds",
+  cardImage:"images/king-of-diamonds.png"
+}
+];
+
 let cardsInPlay = [];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Try again");
 }
 }
 
 function flipCard(cardId) {
-	console.log ("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log ("User flipped " + cards[cardId].rank);
+    console.log ("User flipped " + cards[cardId].suit);
+    console.log ("User flipped " + cards[cardId].cardImage);
+
+	cardsInPlay.push(cards[cardId].rank);
 if (cardsInPlay.length === 2) { 
 	checkForMatch ();
-}
+} 
 }
 
 flipCard(0);
